869d4208e9e62bd35bb786cb5cd6579eb858e1f6
Varun <varunpurohit76@gmail.com>
2016-10-07 01:51:50 +0530

    simple random forest

--- script07oct.1.R
+++ script07oct.1.R
@@ -8,10 +8,10 @@ NA_Count<- data.frame(Item=colnames(train),Count=Num_NA)
 
 #NA_Count
 
-Training<- Training[,-c(7,73,74,75)]
-
+train<- train[,-c(7,73,74,75)]
+test<- test[,-c(7,73,74,75)]
 Num<-sapply(train,is.numeric)
-Num<-Training[,Num]
+Num<-train[,Num]
 
 for(i in 1:77){
   if(is.factor(train[,i])){
@@ -20,6 +20,20 @@ for(i in 1:77){
 }
 
 
-rf <- randomForest(SalePrice~.,data=train)
-predicted <- predict(rf, test)
-print(predicted)
+for(i in 1:76){
+  if(is.factor(test[,i])){
+    test[,i]<-as.integer(test[,i])
+  }
+}
+
+train[is.na(train)]<-0
+test[is.na(test)]<-0
+Num[is.na(Num)]<-0
+
+rf <- randomForest(SalePrice~.,
+                   data=train,
+                   ntree=500)
+submission<- data.frame(Id=test$Id)
+submission$SalePrice <- predict(rf, test)
+
+write.csv(submission, file="script07oct.1.csv", row.names = FALSE)
